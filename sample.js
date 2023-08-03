cf44c993eb0028595c39eb48b19dc08745a75937
Pranav Krishna <pranavkrishnaks1@gmail.com>
2023-08-03 12:20:16 +0530

    init

---
+++ sample.js
@@ -0,0 +1,10 @@
+// a = 0
+// if(a==0){
+//     console.log("The number is 0 and adding 1 making it positive")
+// }
+
+array = [10,20]
+array.forEach(element => {
+    console.log(element)
+    
+});
