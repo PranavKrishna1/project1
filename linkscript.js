d9711dcc6aef53ceab9381a20659848f8ca764ea
Pranav Krishna <pranavkrishnaks1@gmail.com>
2023-08-03 16:44:53 +0530

    init

---
+++ linkscript.js
@@ -0,0 +1,15 @@
+function first() {
+    document.getElementById("hello").innerHTML = "I am in my first year"
+}
+
+function second() {
+    document.getElementById("hello").innerHTML = "I am in my second year"
+}
+
+function third() {
+    document.getElementById("hello").innerHTML = "I am in my third year"
+}
+
+function final() {
+    document.getElementById("hello").innerHTML = "I am in my final year"
+}
--- sample.js
+++ sample.js
@@ -8,3 +8,72 @@ array.forEach(element => {
     console.log(element)
     
 });
+
+console.log("After change:")
+array.push("3rd year")
+for (var ele of array){
+    console.log(ele)
+}
+
+array.pop()
+
+console.log("After pop")
+if (array.length >0){
+    console.log("e")
+}
+
+a = {
+    car:"audi",
+    model:"A8",
+    airbags:8,
+    wheels:5,
+    color:"blue"
+}
+// Array
+const array = [10,20,"PW",23.08 , Date() ]
+console.log("Before change:")
+for (var ele of array) {
+    console.log(ele)
+}
+
+// after
+console.log("After change:")
+array.push("3rd year")
+for (var ele of array) {
+    console.log(ele)
+}
+
+// pop
+array.pop()
+console.log("After pop:")
+
+for (var ele of array) {
+    console.log(ele)
+}
+
+array.pop()
+array.pop()
+array.pop()
+array.pop()
+array.pop()
+
+if (array.length > 0) {
+    console.log ("Elements are in the array")
+    for (var ele of array) {
+        console.log(ele)
+    }
+}
+else {
+    console.log ("No elements are in the array")
+}
+
+array.push(1)
+if (array.length > 0) {
+    console.log ("Elements are in the array")
+    for (var ele of array) {
+        console.log(ele)
+    }
+}
+else {
+    console.log ("No elements are in the array")
+}
